31b21cfbd8597ab9df2bf63ddab3628a00f7d9c5
manuelDev871 <manuelDev871@outlook.es>
2025-10-03 21:16:27 +0200

    update

---
+++ Programación/Tema_1/ProyectoInicial/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* \n hace que haya un salto de linea */
+
+        // TODO me falta esto
+
+        /**
+         * Este comentario
+         * se puede dividir
+         * en varias lineas de codigo
+         */
+
+        System.out.println("Este es mi primer programa en Java\n");
+        System.out.println("Este es la segunda linea de mi programa");
+    }
+
+    public void nombreMetodo() {
+
+    }
+
+    // TODO esto tambien esta pendiente
+
+}
