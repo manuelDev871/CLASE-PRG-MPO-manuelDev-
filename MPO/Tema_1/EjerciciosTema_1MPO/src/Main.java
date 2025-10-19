7106d6de1be3b4f5f8544c6788320bc4df834898
manuelDev871 <manuelDev871@outlook.es>
2025-10-19 17:44:56 +0200

    update

---
+++ MPO/Tema_1/EjerciciosTema_1MPO/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Ejercicio ejercicio = new Ejercicio();
+        // ejercicio.ejercicio1();
+        // ejercicio.ejercicio2();
+        // ejercicio.ejercicio3();
+        // ejercicio.ejercicio4();
+    }
+}
