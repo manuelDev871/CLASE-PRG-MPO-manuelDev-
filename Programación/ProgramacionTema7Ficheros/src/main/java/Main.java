ffc0302530734ee8d4645f4eba56cdf446880fc4
manuelDev871 <manuelDev871@outlook.es>
2026-02-07 13:29:45 +0100

    update

---
+++ Programación/ProgramacionTema7Ficheros/src/main/java/Main.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // File fichero = new File("src/main/java/resources/analisis-secundario.txt");
+        // Logico o fisico?
+        /*
+        System.out.println("Informacion del fichero: ");
+
+        if (fichero.exists()) {
+            System.out.println("Ruta absoluta es: " + fichero.getAbsolutePath());
+            System.out.println("Puede escribit: " + fichero.canWrite());
+            System.out.println("Tamaño fichero: " + fichero.length());
+            System.out.println("Es una carpeta: " + fichero.isDirectory());
+            System.out.println("Es una fichero: " + fichero.isFile());
+        } else {
+            try {
+                fichero.createNewFile();
+                System.out.println("Fichero creado correctamente");
+            } catch (IOException e) {
+                System.out.println("Error en la creacion del fichero");
+                System.out.println(e.getMessage());
+                // solucion -> indica otra ruta para crear el fichero
+                // pongase en contacto con el admin
+            }
+        }
+         */
+
+        File carpeta = new File("src/main/java/resources/carpeta1");
+
+        if (carpeta.isDirectory()) {
+           /* for (String item : carpeta.list()) {
+                System.out.println(item);
+            }
+            */
+
+            for(File item: carpeta.listFiles()) {
+                System.out.println(item.getName());
+                System.out.println(item.isDirectory());
+                // sacar los ficheros de una subcarpeta
+                if (item.isDirectory()) {
+                    for (File data : item.listFiles()) {
+                        System.out.println("\t" + data.getName());
+                        if (data.isDirectory()) {
+                            for (File data1 : data.listFiles()) {
+                                System.out.println("\t \t" + data1.getName());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
